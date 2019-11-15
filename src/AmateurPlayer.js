6c1f5e168194e6ffc2157eb82ef63f3f85f3d7a9
Andres Ochoa <andres_ochoa91@hotmail.com>
2019-11-14 17:12:11 -0800

    some more styles

--- src/AmateurPlayer.js
+++ src/AmateurPlayer.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 const AmateurPlayer = (props) =>{
 	return(
-		<div>
+		<div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+			<img src="https://previews.123rf.com/images/tonok/tonok1604/tonok160400039/56726974-hand-sketch-tennis-player.jpg" width="200px" height="150px" alt="im"/>
 			<h2>Name: {props.nameAmateurPlayer}</h2>
 			<h3>Age: {props.ageAmateurPlayer}</h3>
 			<h3>Amateur</h3>
--- src/ProfessionalPlayersList.js
+++ src/ProfessionalPlayersList.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const ProfessionalPlayersList = (props) =>{
 	return(
-		<div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-			<img src={props.picture} width="200px" alt="im"/>
+		<div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5 fl">
+			<img src={props.picture} width="200px" height="150px" alt="im"/>
 			<h2>Name: {props.name}</h2>
 			<h3>Age: {props.age}</h3>
 			<h3>Grand Slams: {props.grandSlams}</h3>
