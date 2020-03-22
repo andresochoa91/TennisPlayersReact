c8c016a4545f5aab869d1ff55c4defbd99f8d2f4
andresochoa91 <andres_ochoa91@hotmail.com>
2020-03-21 23:31:32 -0700

    some changes and deletes

---
+++ src/containers/App.js
@@ -0,0 +1,70 @@
+import React, {Component} from 'react';
+import Players from '../components/Players';
+import CreationPlayer from '../components/CreationPlayer';
+import "tachyons";
+import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErrorBoundry';
+
+class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      newPlayer: "Alucard",
+      age: "666",
+      newPlayer2: "",
+      age2: "",
+      playerI: []
+    }
+  }
+
+  fet = () =>{
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response=> response.json())
+      .then(users=> this.setState({playerI: users}))  
+  }
+
+  componentDidMount(){
+    this.fet()
+  }
+
+  lookFor = (event) =>{
+    if(event.target.value.length === 0){
+      this.fet()
+    }else{
+      this.setState({playerI: this.state.playerI.filter(
+        pla => pla.name.toLowerCase().includes(event.target.value.toLowerCase())
+      )})      
+    }
+  }
+
+  changeText = (event) =>{
+    this.setState({newPlayer2: event.target.value});
+  }
+
+  changeText2 = (event) =>{
+    this.setState({age2: event.target.value});
+  }
+
+  pushButton = (event) =>{
+    this.setState({newPlayer: this.state.newPlayer2});
+    this.setState({age: this.state.age2});    
+  }
+
+  render(){
+    const {newPlayer, age, playerI} = this.state;
+    return(
+      <div className="tc dib br3 pa3 ma2 bw2 fl w-100">
+        <h1 className="f1">Tennis Players</h1>
+        <input type="search" placeholder="Look for the tennis player" onChange={this.lookFor}/>
+        <Scroll>
+          <ErrorBoundry>
+            <Players nameAmateurPlayer={newPlayer} ageAmateurPlayer={age} PlayersInfo={playerI}/>
+          </ErrorBoundry>
+        </Scroll>        
+        <CreationPlayer onChangeText={this.changeText} onChangeText2={this.changeText2} onClick={this.pushButton}/>
+      </div>
+    )   
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './CloneApp/containers/App';
+import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 import { Provider, connect } from 'react-redux';
 import { createStore } from 'redux';
