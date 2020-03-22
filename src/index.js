62ea387963de9e8e72ebc715603e38096fd3fdfa
andresochoa91 <andres_ochoa91@hotmail.com>
2020-03-21 18:40:16 -0700

    redux added, If something goes wrong, go back to the last repositoty

--- src/index.js
+++ src/index.js
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './CloneApp/containers/App';
 import * as serviceWorker from './serviceWorker';
+import { Provider, connect } from 'react-redux';
+import { createStore } from 'redux';
+import { searchPlayers } from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(searchPlayers)
+
+ReactDOM.render(<App store={ store } />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
